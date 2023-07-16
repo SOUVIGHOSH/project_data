20d44d8120956b3bccaa4e282df77fc6259d4601
codefoliohub <21f1003361@student.onlinedegree.iitm.ac.in>
2023-07-17 00:50:15 +0530

    Added cors

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const jsonServer = require("json-server");
+const cors = require("cors");
+
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 8080;
+
+// Enable CORS for all origins
+server.use(cors());
 
 server.use(middlewares);
 server.use(router);
